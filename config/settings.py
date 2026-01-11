b1866c6305a12c20c11d1e737b36acbf8eeaa553
Ксения Хамитова <ksushahamitova@gmail.com>
2026-01-11 22:01:25 +0500

    Deploy fix: remove duplicate STATIC settings

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,20 +124,14 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Для деплоя (позже раскомментируем):
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if 'RENDER' in os.environ:
-    # Режим продакшена
     DEBUG = False
-    
-    # Хост Render
     ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME'), 'localhost']
     
-    # PostgreSQL от Render
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(
@@ -146,7 +139,3 @@ if 'RENDER' in os.environ:
             conn_max_age=600
         )
     }
-    
-    # Статика
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
