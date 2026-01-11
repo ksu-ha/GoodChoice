dda66f9e0d768b37903d8ecf38f39411d533c91e
Ксения Хамитова <ksushahamitova@gmail.com>
2026-01-11 22:11:30 +0500

    fix: use Path for STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 if 'RENDER' in os.environ:
